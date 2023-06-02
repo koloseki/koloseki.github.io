84d48f4c14a13390f782dfb31805cece1273630e
koloseki <koloseki2002@gmail.com>
2023-06-02 16:05:15 +0200

    Photos now should working on a gh-pages

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -12,6 +12,10 @@ import vite_logo from '/src/assets/SkillsSvg/vite-logo.svg';
 import bootstrap_logo from '/src/assets/SkillsSvg/bootstrap-logo.svg';
 import live_logo from '/src/assets/SkillsSvg/live-logo.svg';
 import faChevronUp from '/src/assets/faChevronUp.svg';
+import WeatherAppPhoto from '/public/weather-laptop.jpg';
+import PomidorPhoto from '/public/pomidro-mobile.jpg';
+import EtchaSketchPhoto from '/public/EtchASketch.jpg';
+
 
 const Skill = "Skill";
 
@@ -33,21 +37,21 @@ export const Content = () => {
 	useEffect(() => {
 		switch (PhotoStatus) {
 			case 0:
-				setMainPhoto('./src/assets/Projects_Photos/weather-laptop.jpg');
+				setMainPhoto(WeatherAppPhoto);
 				setTitle ("Weather App");
 				setGithubLink('https://github.com/koloseki/Weather_App');
 				setLiveLink('http://koloseki.me/Weather_App/');
 				setDescription('Weather App is a user-friendly application designed to provide accurate and up-to-date weather information. Powered by a robust Weather API, it offers comprehensive data on current weather conditions, forecasts, and more for any location worldwide. Whether planning a trip, staying informed about local weather, or simply curious about weather patterns around the globe, Weather App ensures users stay ahead of the elements. Stay prepared and make informed decisions with Weather App, your reliable companion for all things weather-related.')
 				break;
 			case 1:
-				setMainPhoto('./src/assets/Pomidor_Photo.png');
+				setMainPhoto(PomidorPhoto);
 				setTitle("Pomidor");
 				setGithubLink('https://github.com/koloseki/pomidor');
 				setLiveLink('http://koloseki.me/pomidor/');
 				setDescription('Pomidor is a simple and efficient Pomodoro timer application built on React. Inspired by the popular time management technique, Pomidor helps users maximize productivity and focus by breaking tasks into manageable intervals. With its clean and minimalistic interface, the app allows users to set custom work and break durations, creating a personalized work rhythm. As the timer counts down, Pomidor provides  audio cue to indicate when it\'s time to switch between work and break sessions. This seamless experience ensures users stay on track and maintain a healthy work-life balance. Boost your productivity and conquer your tasks with Pomidor, the ultimate companion for effective time management. ')
 				break;
 			case 2:
-				setMainPhoto('./src/assets/EtchSketch_Photo.png');
+				setMainPhoto(EtchaSketchPhoto);
 				setTitle("Etch a Sketch");
 				setGithubLink('https://github.com/koloseki/etchaSketch');
 				setLiveLink('http://koloseki.me/etchaSketch/');
@@ -81,7 +85,7 @@ export const Content = () => {
 			<main>
 				<div className='About-me'>
 					<h2 className="Flip">About Me</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium lectus arcu, eu facilisis sapien ultrices ac. Duis eleifend tempus augue, ac sollicitudin lorem pharetra sed. Vivamus tincidunt eget odio quis mattis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget justo ipsum. Pellentesque mauris massa, imperdiet ac varius eu, bibendum non sem. Nunc placerat congue convallis. Maecenas eu magna cursus, ullamcorper diam lobortis, eleifend quam. Vivamus ultrices imperdiet orci, scelerisque malesuada ipsum interdum sollicitudin. Donec lacus turpis, scelerisque ac nunc ut, commodo gravida ex. Nam orci metus, convallis non convallis a, aliquet fermentum lacus. Vestibulum sagittis diam at nunc sagittis, a egestas diam vulputate.  </p>
+					<p>I am a Front End Developer based in Poland. I have a passion for the creative process of building visually appealing designs and interactive user interfaces. Constantly seeking opportunities to learn new concepts, technologies, and tools, I strive to create the best possible user experience. My focus lies in programming and design, aiming to craft aesthetic and intuitive solutions. I am dedicated to expanding my skill set to stay up-to-date with the latest industry trends. Collaboration and working on innovative team projects excite me. With a keen eye for detail and a commitment to excellence, I approach each task with enthusiasm and strive for perfection in every project I undertake. </p>
 				</div>
 
 				<h2 className="Content">Skills</h2>
